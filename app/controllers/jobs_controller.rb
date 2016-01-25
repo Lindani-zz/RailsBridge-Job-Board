b5dcd782ef373b6c93381ed38db00885b0482a03
lindani <lindani@projectcodex.co>
2016-01-25 11:30:19 +0200

    Added a method that captures data entry to be created and stored

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,5 +2,23 @@ class JobsController < ApplicationController
 	def index
 	end
 	def new
+		@job = Job.new
+
+	end
+
+	#Creating a new job entry
+	def create
+		p "iN THE CREATE METHOD!!!!"
+		job = Job.create(job_params)
+		p job
+		redirect_to jobs_path
+	end
+
+	#Using strong paremeters to limit whatever data our form can submit to our app
+
+	private
+
+	def job_params
+	  params.require(:job).permit(:title, :description)
 	end
 end
