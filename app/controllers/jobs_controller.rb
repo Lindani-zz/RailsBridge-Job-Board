66b22b81acf5a70e8df3d70cafde1d0c5898df7e
lindani <lindani@projectcodex.co>
2016-01-25 13:21:42 +0200

    added destroy method

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,9 +4,21 @@ class JobsController < ApplicationController
 	end
 	def new
 		@job = Job.new
-
 	end
 
+	def edit
+		@job = Job.find(params[:id])
+	end
+	def update
+		@job = Job.find(params[:id])
+  		@job.update_attributes(job_params)
+		redirect_to jobs_path
+	end
+	def destroy
+		@job = Job.find(params[:id])
+  		@job.destroy
+		redirect_to jobs_path
+	end
 	#Creating a new job entry
 	def create
 		p "IN THE CREATE METHOD!!!!"
