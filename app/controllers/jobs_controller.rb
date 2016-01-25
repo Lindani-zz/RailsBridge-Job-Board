2e8aa9d2dd1efa8ba8ede4aeba8e6fbd17d6ae0a
lindani <lindani@projectcodex.co>
2016-01-25 11:40:40 +0200

    showing all jobs on jobs page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
 	def index
+		@jobs = Job.all
 	end
 	def new
 		@job = Job.new
@@ -8,7 +9,7 @@ class JobsController < ApplicationController
 
 	#Creating a new job entry
 	def create
-		p "iN THE CREATE METHOD!!!!"
+		p "IN THE CREATE METHOD!!!!"
 		job = Job.create(job_params)
 		p job
 		redirect_to jobs_path
