b1183c224a14cee8d141c8fcb88a1a91c3cf417c
lindani <lindani@projectcodex.co>
2016-01-26 13:40:14 +0200

    added table with styling and populated seed file with dump data

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'quiet_assets'
+gem 'seed_dump'
+
 
 
 # Use ActiveModel has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Job.create!([
+  {title: "Engineering", description: "Software engineer ", requirements: "non, just Github account deatails"},
+  {title: "y5t5", description: "t5wt", requirements: "5te4t4t"},
+  {title: "544w", description: "gfttr", requirements: "tstgsrtg"},
+  {title: "sgse", description: "ggst", requirements: "stgtsg"}
+])
+Job.create!([
+  {title: "Engineering", description: "Software engineer ", requirements: "non, just Github account deatails"},
+  {title: "y5t5", description: "t5wt", requirements: "5te4t4t"},
+  {title: "544w", description: "gfttr", requirements: "tstgsrtg"},
+  {title: "sgse", description: "ggst", requirements: "stgtsg"}
+]).times(50)
