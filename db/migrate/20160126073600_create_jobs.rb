302ea88455572b42b1ae6eb40d0edc037f01dc26
lindani <lindani@projectcodex.co>
2016-01-26 10:05:30 +0200

    Add show page link and modigied db to have requirements

--- db/migrate/20160126073600_create_jobs.rb
+++ db/migrate/20160126073600_create_jobs.rb
@@ -1,10 +1,10 @@
 class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
-
-    	t.text :title
-    	t.text :description
-    	t.timestamps null: false
+    t.text :title
+  	t.text :description
+  	t.text :requirements
+    t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125084024) do
+ActiveRecord::Schema.define(version: 20160126073600) do
 
   create_table "jobs", force: :cascade do |t|
     t.text     "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "requirements"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
