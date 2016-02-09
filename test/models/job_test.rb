c429305bc2d4a29983c9dc38340d4e5552bb945e
lindani <lindani@projectcodex.co>
2016-02-09 13:58:33 +0200

    Passed test for validating title and descriptton

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -4,6 +4,13 @@ class JobTest < ActiveSupport::TestCase
  
   test "should not save new job without title" do
   	job = Job.new
-  	assert_not job.save, "Saved the job without the title"
+  	job.valid?
+  	assert job.errors.keys.include?(:title), "Saved the job without the title"
+  end
+ 
+  test "job should have description" do
+	 	job = Job.new
+	 	job.valid?
+	 	assert job.errors.keys.include?(:description), "Saved the job description"
   end
 end
