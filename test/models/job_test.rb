65a30baf6a3388f1e47c6eb4ae36b7ca8ef9f6b2
lindani <lindani@projectcodex.co>
2016-02-09 11:48:28 +0200

    saving job without title faluire

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -4,8 +4,8 @@ class JobTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
-  test "should not save new job with title" do
+  test "should not save new job without title" do
   	job = Job.new
-  	assert_not job.save
+  	assert_not job.save, "Saved the job without the title"
   end
 end
