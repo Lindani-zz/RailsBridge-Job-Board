9a3451636f4ac5f8280bbe1a25c8d59bad8f18a6
lindani <lindani@projectcodex.co>
2016-02-09 16:34:22 +0200

    validate job requirements passing tests

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,16 +1,22 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
- 
-  test "should not save new job without title" do
+
+  test "job should have title" do
   	job = Job.new
-  	job.valid?
-  	assert job.errors.keys.include?(:title), "Saved the job without the title"
+		job.valid?
+  	assert job.errors.keys.include?(:title), "Saved the job with the title"
   end
- 
+
   test "job should have description" do
-	 	job = Job.new
-	 	job.valid?
+  	job = Job.new
+		job.valid?
 	 	assert job.errors.keys.include?(:description), "Saved the job description"
+  end
+
+  test "job should have requirements" do
+  	job = Job.new
+		job.valid?
+  	assert job.errors.keys.include?(:requirements), "Saved the job requirements"
   end
 end
