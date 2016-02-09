2f7d8f4be0e36654e23d4861c60cf404e24c2a0b
lindani <lindani@projectcodex.co>
2016-02-09 11:09:42 +0200

    Passing test for models

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,7 +1,11 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class JobTest < ActiveSupport::TestCase
-#   # test "the truth" do
-#   #   assert true
-#   # end
-# end
+class JobTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert true
+  end
+  test "should not save new job with title" do
+  	job = Job.new
+  	assert_not job.save
+  end
+end
