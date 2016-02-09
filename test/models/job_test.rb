97c84163e897303ffe27a90cf8a30598277b689b
lindani <lindani@projectcodex.co>
2016-02-09 12:00:47 +0200

    validated model attributes to make the model functional test pass

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
+ 
   test "should not save new job without title" do
   	job = Job.new
   	assert_not job.save, "Saved the job without the title"
