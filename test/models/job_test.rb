9abd00b176de5c4917c461b6b48af1d85a677baa
lindani <lindani@projectcodex.co>
2016-02-10 13:17:54 +0200

    Testing model methods

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -19,4 +19,9 @@ class JobTest < ActiveSupport::TestCase
 		job.valid?
   	assert job.errors.keys.include?(:requirements), "Saved the job requirements"
   end
+
+  test "job should have a list of tags" do 
+  	job = Job.new
+  	assert_equal(tagged_with(), "don't know")
+  end
 end
